c7ef6da17ec797cebb21097364241c54429849d3
CaXaR777 <116805047+CaXaR777@users.noreply.github.com>
2024-03-20 21:53:38 +0000

    Deploying to gh-pages from @ CaXaR777/GoIt-TA@6e2a6e53c41f120857a5d9da6a474917a8e0bf05 🚀

---
+++ assets/CatalogPage-CmMbREFl.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DO1kNxng.js";const r=()=>t.jsx(t.Fragment,{children:t.jsx("p",{children:"Catalog"})});export{r as default};
---
+++ assets/FavoritesPage-CyCcSzYM.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DO1kNxng.js";const t=()=>e.jsx(e.Fragment,{children:e.jsx("p",{className:"text-orange-500",children:"Favorites"})});export{t as default};
